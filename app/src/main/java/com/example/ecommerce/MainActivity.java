ae68b0d9e0fddd64957f59820e0f4aafe68ca6e7
FOODOHOLIC <adarshkohli183@gmail.com>
2022-07-01 21:28:46 +0530

    Initial commit

--- app/src/main/java/com/example/ecommerce/MainActivity.java
+++ app/src/main/java/com/example/ecommerce/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.ecommerce;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -15,17 +16,14 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         joinNowButton = (Button) findViewById(R.id.main_join_now_btn);
-        joinNowButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-            }
-        });
         loginButton = (Button) findViewById(R.id.main_login_btn);
+
+
         loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                Intent intent = new Intent(MainActivity.this,LoginActivity.class);
+                startActivity(intent);
             }
         });
     }
